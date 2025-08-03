d0285e9feab356c6b524d02a18b50d8f865a4050
Ancil <24bcs015@nith.ac.in>
2025-08-03 23:39:24 +0530

    update

--- lab_1.c
+++ lab_1.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 void method1(float arr2[], int size){
+
+  int start_time = clock();
+
   float sum = 0;
   float q = 0;
-  int arr1[size];
+  float *arr1 = malloc(size * sizeof(float));
   for(int i=0;i<size;i++){
     arr1[i] = (rand() % 100);
   }
@@ -12,13 +17,20 @@ void method1(float arr2[], int size){
     q = sum/(i+1);
     arr2[i] = q;
   }
+
+  int end_time = clock();
+  double time_taken = ((double)(end_time - start_time))/CLOCKS_PER_SEC;
+  printf("%d, %f \n", size, time_taken);
 }
 
 void method2(float arr2[], int size){
+  
+  int start_time = clock();
+
   float q = 0;
-  int arr1[size];
+  float *arr1 = malloc(size * sizeof(float));
   for(int i=0;i<size;i++){
-    arr1[i] = (rand() % 100);
+    arr1[i] = (rand() % 1000);
   }
   for(int i=0;i<size;i++){
     float sum = 0;
@@ -28,14 +40,16 @@ void method2(float arr2[], int size){
     q = sum/(float)(i+1);
     arr2[i] = q;
   }
+  int end_time = clock();
+  double time_taken = ((double)(end_time - start_time))/ CLOCKS_PER_SEC;
+  printf("%d, %f \n ", size, time_taken);
 }
 
 int main(){
-  int size = (rand() % 100);
-  float finalArray[size];
+  srand(time(NULL));
+  int size = (rand() % 10000000);
+  float *finalArray = malloc(size * sizeof(float));
   method1(finalArray,size);
-   for (int i = 0; i < size; i++) {
-        printf("%f ",finalArray[i]);
-    }
+
   return 0;
 }
