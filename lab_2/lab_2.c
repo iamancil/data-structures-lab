6b752b04bd62fa9a7700c86406e341f8f26ec9ee
Ancil <24bcs015@nith.ac.in>
2025-08-06 12:26:17 +0530

    lab_1 updated code

---
+++ lab_2/lab_2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(){
+  int size;
+  printf("What is the size of the array? ");
+  scanf("%d", &size);
+  int* arr = malloc(sizeof(int[size]));
+  for(int i=0;i<size;i++){
+    printf("Enter %d th element: ", i);
+    scanf("%d", &arr[i]);
+  }
+
+  for(int i=0;i<size-1;i++){
+    bool flag = false;
+    for(int j=0;j<size-i-1;j++){
+      if(arr[j]>arr[j+1]){
+        int temp;
+        temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        flag = true;
+      }
+    }
+    if(flag == false){
+        break;
+      }  
+  }
+  printf("Elements after the sorting:\n ");
+  for(int i=0;i<size;i++){
+    printf("%d \n", arr[i]);
+  }
+  printf("Enter the element to find");
+  int var;
+  scanf("%d", &var);
+  for(int i=0;i<size;i++){
+    if(arr[i] == var){
+      printf("The number is located at %d index", i);
+    }
+  }
+}
+
+void main(){
+  bubbleSort();
+}
