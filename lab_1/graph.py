16f75cc1a6532f2a9378a32aa7a75ad0e5d20f66
Ancil <24bcs015@nith.ac.in>
2025-08-08 09:21:32 +0530

    updated the graphs in lab_1

--- lab_1/graph.py
+++ lab_1/graph.py
@@ -2,11 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd
 
+fig, x1 = plt.subplots()
+fig, x2 = plt.subplots()
+
 data = pd.read_csv("result.csv")
-plt.plot(data['n'], data['time On'], marker = 'o', label = 'On method')
-plt.plot(data['n'], data['time On_2'], marker = 'x', label = 'On_2 method')
+x1.plot(data['n'], data['time On'], marker = '.', label = 'On method')
+x2.plot(data['n'], data['time On_2'], marker = '.', label = 'On_2 method')
+x2.set_xlabel("No. of Inputs")
+x2.set_ylabel("Time")
 
+x1.set_xlabel("No. of Inputs")
+x1.set_ylabel("Time")
 
-plt.xlabel("No. of Inputs")
-plt.ylabel("Time")
 plt.show()
